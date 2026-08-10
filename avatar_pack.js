74fdc7e831fcc2c7edd7988894d27cfcc855c8ab
jiuxi908-rgb <jiuxi908@gmail.com>
2026-08-10 23:26:24 +0800

    清理：删除未使用的 avatar_pack 桩文件

--- avatar_pack.js
+++
@@ -1,6 +0,0 @@
-/** 像素头像包 · 分片加载后可用 */
-(function(g){
-  g.AVATAR_PACK=g.AVATAR_PACK||Object.create(null);
-  g.avatarPackReady=function(){return Object.keys(g.AVATAR_PACK).length>0;};
-  g.whenAvatarPack=function(fn){fn(g.AVATAR_PACK);};
-})(typeof window!=="undefined"?window:this);
